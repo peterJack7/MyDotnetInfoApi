9458e3b6ec5c1fbd82318cac1910187feb9afa5a
peterJack7 <pnj90047@gmail.com>
2025-04-13 19:39:56 +0530

    Add project files.

---
+++ MyDotnetInfoApi/Controllers/InfoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyDotnetInfoApi.Models;
+
+namespace MyDotnetInfoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<InfoResponse> GetInfo()
+        {
+            var response = new InfoResponse
+            {
+                Name = "My Dotnet App",
+                Version = "1.0.0",
+                Status = "Running"
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ MyDotnetInfoApi/Models/InfoResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MyDotnetInfoApi.Models
+{
+    public class InfoResponse
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Status { get; set; }
+    }
+}
